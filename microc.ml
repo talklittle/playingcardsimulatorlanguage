df36d7db08b4839f91d06c2c90ee4c641c59b278
ehenestroza <ehenestroza@63fd7cd2-9ecc-11dd-af58-c1d6d257e395>
2008-11-11 21:19:02 +0000

    Sample interpreter to build from.

---
+++ microc.ml
@@ -0,0 +1,10 @@
+let print = false
+
+let _ =
+  let lexbuf = Lexing.from_channel stdin in
+  let program = Parser.program Scanner.token lexbuf in
+  if print then
+    let listing = Printer.string_of_program program in  
+    print_string listing
+  else
+    ignore (Interpret.run program)
