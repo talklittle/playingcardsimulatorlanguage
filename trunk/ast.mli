f51a7be880e2194d9fd222225a08f96746a9a149
ehenestroza <ehenestroza@63fd7cd2-9ecc-11dd-af58-c1d6d257e395>
2008-11-15 03:49:24 +0000

    Functional scanner, parser, ast, and printer to check that ast from an input program is correct.

--- trunk/ast.mli
+++ trunk/ast.mli
@@ -1,26 +1,88 @@
-type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq
+type bool = True | False
+
+type op = 
+  Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq | And | Or
+
+type scope =
+    None
+  | Global
+  | Local
+  | Some of string
+
+type t =
+    Unknown
+  | Int
+  | StringType
+  | Bool
+  | CardEntity
+  | Card
+
+type var = Var of string * scope * t
 
 type expr =
-    Literal of int
-  | Id of string
+    Null
+  | IntLiteral of int
+  | StringLiteral of string
+  | BoolLiteral of bool
+  | Id of var
   | Binop of expr * op * expr
-  | Assign of string * expr
-  | Call of string * expr list
+  | Assign of expr * expr
+  | Transfer of expr * expr
+  | Call of var * expr list
   | Noexpr
 
 type stmt =
-    Block of stmt list
+    Break
+  | Print of expr
+  | Read of expr
   | Expr of expr
   | Return of expr
-  | If of expr * stmt * stmt
-  | For of expr * expr * expr * stmt
-  | While of expr * stmt
+  | If of expr * stmt list * stmt list
+  | For of expr * expr * expr * stmt list
+  | While of expr * stmt list
+  | Nostmt
 
 type func_decl = {
-    fname : string;
+    fname : var;
     formals : string list;
-    locals : string list;
+    locals : var list;
     body : stmt list;
   }
 
-type program = string list * func_decl list
+type incl_decl = {
+    includes : string list;
+  }
+
+type cent_decl = {
+    entities : string list;
+  }
+
+type glob_decl = {
+    globals : var list;
+  }
+
+type strt_decl = {
+    slocals : var list;
+    sbody : stmt list;
+  }
+
+type play_decl = {
+    plocals : var list;
+    pbody : stmt list;
+  }
+
+type wcon_decl = {
+    wlocals : var list;
+    wbody : stmt list;
+  }
+
+type spec_decl = {
+    incl : incl_decl;
+    cent : cent_decl;
+    glob : glob_decl;
+    strt : strt_decl;
+    play : play_decl;
+    wcon : wcon_decl;
+  }
+
+type program = spec_decl * func_decl list
--- trunk/printer.ml
+++ trunk/printer.ml
@@ -1,41 +1,106 @@
 open Ast
 
+let string_of_op op = match op with
+    Add     -> "+" 
+  | Sub     -> "-" 
+  | Mult    -> "*" 
+  | Div     -> "/"
+  | Equal   -> "==" 
+  | Neq     -> "!="
+  | Less    -> "<" 
+  | Leq     -> "<=" 
+  | Greater -> ">" 
+  | Geq     -> ">="
+  | And     -> "&&"
+  | Or      -> "||"
+
+let string_of_bool bool = match bool with
+    True  -> "true"
+  | False -> "false"
+
+let string_of_t t = match t with
+    Unknown    -> ""
+  | Int        -> "int "
+  | StringType -> "string "
+  | Bool       -> "bool "
+  | CardEntity -> "$"
+  | Card       -> "Card "
+
+let string_of_scope scope = match scope with
+    None    -> ""
+  | Global  -> "#"
+  | Local   -> ""
+  | Some(s) -> "$" ^ s ^ "."
+			   
+let string_of_var v = match v with
+  Var(id, scope, t) -> string_of_t t ^ string_of_scope scope ^ id
+
 let rec string_of_expr = function
-    Literal(l) -> string_of_int l
-  | Id(s) -> s
+    Null -> "null"
+  | IntLiteral(l) -> string_of_int l
+  | StringLiteral(l) -> l
+  | BoolLiteral(b) -> string_of_bool b
+  | Id(v) -> string_of_var v
   | Binop(e1, o, e2) ->
-      string_of_expr e1 ^ " " ^
-      (match o with
-	Add -> "+" | Sub -> "-" | Mult -> "*" | Div -> "/"
-      | Equal -> "==" | Neq -> "!="
-      | Less -> "<" | Leq -> "<=" | Greater -> ">" | Geq -> ">=") ^ " " ^
-      string_of_expr e2
-  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Assign(e1, e2) -> string_of_expr e1 ^ " <- " ^ string_of_expr e2
+  | Transfer(e1, e2) -> string_of_expr e1 ^ " -> " ^ string_of_expr e2
   | Call(f, el) ->
-      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+      string_of_var f ^ "(" ^ 
+      String.concat ", " (List.map string_of_expr el) ^ ")"
   | Noexpr -> ""
 
 let rec string_of_stmt = function
-    Block(stmts) ->
-      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
-  | Expr(expr) -> string_of_expr expr ^ ";\n";
-  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
-  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
-  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
-      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+    Break -> "break;\n"
+  | Print(expr) -> string_of_expr expr ^ " >>\n"
+  | Read(expr) -> string_of_expr expr ^ " <<\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n"
+  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n"
+  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ") {\n" ^
+      String.concat "" (List.map string_of_stmt s1) ^ "} else {\n" ^ 
+      String.concat "" (List.map string_of_stmt s2) ^ "}\n"
   | For(e1, e2, e3, s) ->
       "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
-      string_of_expr e3  ^ ") " ^ string_of_stmt s
-  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+      string_of_expr e3  ^ ") {\n" ^ 
+      String.concat "" (List.map string_of_stmt s) ^ "}\n"
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") {\n" ^ 
+      String.concat "" (List.map string_of_stmt s) ^ "}\n"
+  | Nostmt -> ""
 
-let string_of_vdecl id = "int " ^ id ^ ";\n"
+let string_of_strdecl id = id ^ ";\n"
+				  
+let string_of_vdecl v = 
+  string_of_var v ^ ";\n"
 
 let string_of_fdecl fdecl =
-  fdecl.fname ^ "(" ^ String.concat ", " fdecl.formals ^ ")\n{\n" ^
+  string_of_var fdecl.fname ^ "(" ^ 
+  String.concat ", " fdecl.formals ^ ")\n{\n" ^
   String.concat "" (List.map string_of_vdecl fdecl.locals) ^
   String.concat "" (List.map string_of_stmt fdecl.body) ^
   "}\n"
 
-let string_of_program (vars, funcs) =
-  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+let string_of_sdecl_1 sname strings =
+  sname ^ "\n{\n" ^
+  String.concat "" (List.map string_of_strdecl strings) ^
+  "}\n"
+
+let string_of_sdecl_2 sname vars =
+  sname ^ "\n{\n" ^
+  String.concat "" (List.map string_of_vdecl vars) ^
+  "}\n"
+
+let string_of_sdecl_3 sname vars body =
+  sname ^ "\n{\n" ^
+  String.concat "" (List.map string_of_vdecl vars) ^
+  String.concat "" (List.map string_of_stmt body) ^
+  "}\n"
+
+let string_of_program (spec, funcs) =
+  string_of_sdecl_1 "Include" spec.incl.includes ^ "\n" ^
+  string_of_sdecl_1 "CardEntities" spec.cent.entities ^ "\n" ^
+  string_of_sdecl_2 "Globals" spec.glob.globals ^ "\n" ^
+  string_of_sdecl_3 "Start" spec.strt.slocals spec.strt.sbody ^ "\n" ^
+  string_of_sdecl_3 "PlayOrder" spec.play.plocals spec.play.pbody ^ "\n" ^
+  string_of_sdecl_3 "WinningCondition" spec.wcon.wlocals spec.wcon.wbody 
+  ^ "\n" ^
   String.concat "\n" (List.map string_of_fdecl funcs)
