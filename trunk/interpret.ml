7bb8623cc202a52c96405dad01e6db66205d6e23
talklittle <talklittle@63fd7cd2-9ecc-11dd-af58-c1d6d257e395>
2008-12-13 17:24:43 +0000

    Update definitions of literals, store as strings internally.
    Delete extra level of definition "Literal", replace with the subtypes
    IntLiteral CardLiteral BoolLiteral StringLiteral directly.
    
    Delete redundant definition of "bool" since OCAML has bool type already.
    
    Still working on the interpret eval block. No compile errors for parser and ast.

--- trunk/interpret.ml
+++ trunk/interpret.ml
@@ -23,10 +23,13 @@ let run (vars, funcs) =
   (* FIXME the types are really screwed up *)
   let rec eval env = function
 
-      Null -> 1, env  (* FIXME what to return for null? *)
-    | Noexpr -> 1, env (* must be non-zero for the loop predicate *)
+      Null -> "null", env  (* XXX ok to return "null" here? *)
+    | Noexpr -> "1", env (* must be non-zero for the loop predicate *)
 
-    | Literal(i) -> i, env
+    | IntLiteral(i) -> i, env
+    | StringLiteral(i) -> i, env
+    | BoolLiteral(i) -> i, env
+    | CardLiteral(i) -> i, env
 
     (* Return (list of evaluated expressions), env *)
     (* Applicative order: evaluate each argument, updating env each time *)
@@ -51,39 +54,44 @@ let run (vars, funcs) =
     | Binop(e1, op, e2) ->
         let v1, env = eval env e1 in
         let v2, env = eval env e2 in
-        let boolean i = if i then 1 else 0 in
+        let boolean i = if i then "1" else "0" in
         (match v1, op, v2 with
-          IntLiteral, Add, IntLiteral -> v1 + v2
-        | IntLiteral, Sub, IntLiteral -> v1 - v2
-        | IntLiteral, Mult, IntLiteral -> v1 * v2
-        | IntLiteral, Div, IntLiteral -> v1 / v2
-        | IntLiteral, Equal, IntLiteral -> boolean (v1 = v2)
+          IntLiteral, Add, IntLiteral -> string_of_int(int_of_string v1 + int_of_string v2)
+        | IntLiteral, Sub, IntLiteral -> string_of_int(int_of_string v1 - int_of_string v2)
+        | IntLiteral, Mult, IntLiteral -> string_of_int(int_of_string v1 * int_of_string v2)
+        | IntLiteral, Div, IntLiteral -> string_of_int(int_of_string v1 / int_of_string v2)
+
+        | IntLiteral, Equal, IntLiteral -> boolean (int_of_string v1 = int_of_string v2)
         | StringLiteral, Equal, StringLiteral -> boolean (v1 = v2)
         | CardLiteral, Equal, CardLiteral -> boolean (v1 = v2)
-        | BoolLiteral, Equal, BoolLiteral -> boolean (string_of_bool v1 = string_of_bool v2)
-        | IntLiteral, Neq, IntLiteral -> boolean (v1 <> v2)
+        | BoolLiteral, Equal, BoolLiteral -> boolean (v1 = v2)
+        | IntLiteral, Neq, IntLiteral -> boolean (int_of_string v1 <> int_of_string v2)
         | StringLiteral, Neq, StringLiteral -> boolean (v1 <> v2)
         | CardLiteral, Neq, CardLiteral -> boolean (v1 <> v2)
-        | BoolLiteral, Neq, BoolLiteral -> boolean (string_of_bool v1 <> string_of_bool v2)
-        | IntLiteral, Less, IntLiteral -> boolean (v1 < v2)
+        | BoolLiteral, Neq, BoolLiteral -> boolean (v1 <> v2)
+
+        | IntLiteral, Less, IntLiteral -> boolean (int_of_string v1 < int_of_string v2)
         | StringLiteral, Less, StringLiteral -> boolean (v1 < v2)
-        | CardLiteral, Less, CardLiteral -> boolean (v1 < v2)
-        | BoolLiteral, Less, BoolLiteral -> boolean (string_of_bool v1 < string_of_bool v2)
-        | IntLiteral, Leq, IntLiteral -> boolean (v1 <= v2)
+        | CardLiteral, Less, CardLiteral -> boolean (v1 < v2) (* XXX ok compare cards as strings? *)
+        | BoolLiteral, Less, BoolLiteral -> boolean (v1 < v2)
+        | IntLiteral, Leq, IntLiteral -> boolean (int_of_string v1 <= int_of_string v2)
         | StringLiteral, Leq, StringLiteral -> boolean (v1 <= v2)
-        | CardLiteral, Leq, CardLiteral -> boolean (v1 <= v2)
-        | BoolLiteral, Leq, BoolLiteral -> boolean (string_of_bool v1 <= string_of_bool v2)
-        | IntLiteral, Greater, IntLiteral -> boolean (v1 > v2)
+        | CardLiteral, Leq, CardLiteral -> boolean (v1 <= v2) (* XXX ok compare cards as strings? *)
+        | BoolLiteral, Leq, BoolLiteral -> boolean (v1 <= v2)
+        | IntLiteral, Greater, IntLiteral -> boolean (int_of_string v1 > int_of_string v2)
         | StringLiteral, Greater, StringLiteral -> boolean (v1 > v2)
-        | CardLiteral, Greater, CardLiteral -> boolean (v1 > v2)
-        | BoolLiteral, Greater, BoolLiteral -> boolean (string_of_bool v1 > string_of_bool v2)
-        | IntLiteral, Geq, IntLiteral -> boolean (v1 >= v2)
+        | CardLiteral, Greater, CardLiteral -> boolean (v1 > v2) (* XXX ok compare cards as strings? *)
+        | BoolLiteral, Greater, BoolLiteral -> boolean (v1 > v2)
+        | IntLiteral, Geq, IntLiteral -> boolean (int_of_string v1 >= int_of_string v2)
         | StringLiteral, Geq, StringLiteral -> boolean (v1 >= v2)
-        | CardLiteral, Geq, CardLiteral -> boolean (v1 >= v2)
-        | BoolLiteral, Geq, BoolLiteral -> boolean (string_of_bool v1 >= string_of_bool v2)
-        | BoolLiteral, And, BoolLiteral -> boolean (v1 && v2)
-        | BoolLiteral, Or, BoolLiteral -> boolean (v1 || v2)
+        | CardLiteral, Geq, CardLiteral -> boolean (v1 >= v2) (* XXX ok compare cards as strings? *)
+        | BoolLiteral, Geq, BoolLiteral -> boolean (v1 >= v2)
+
+        | BoolLiteral, And, BoolLiteral -> boolean (bool_of_string v1 && bool_of_string v2)
+        | BoolLiteral, Or, BoolLiteral -> boolean (bool_of_string v1 || bool_of_string v2)
+
         | StringLiteral, Concat, StringLiteral -> v1 ^ v2  (* XXX we want String concat, right? *)
+
         | _, _, _ ->
             raise (Failure ("invalid binary operation"))
         ), env
