4fe83f8892c3cebd7a75b9393a9ebc5b76aa3411
ehenestroza <ehenestroza@63fd7cd2-9ecc-11dd-af58-c1d6d257e395>
2008-12-19 09:31:30 +0000

    Fixed Include block functionality.

--- pcgsl.ml
+++ pcgsl.ml
@@ -1,11 +1,30 @@
-let print = false
+let print = true;;
 
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program Scanner.token lexbuf in
-  (*if print then 
-    let listing = Printer.string_of_program program in  
+let filename = 
+  if Array.length Sys.argv != 2
+  then 
+    (print_string "Usage: ./pcgsl sourcefile\n";
+    exit 1)
+  else Sys.argv.(1);;
+let chan = open_in filename;;
+let buf = String.create (in_channel_length chan);;
+let a = really_input chan buf 0 (String.length buf);;
+let lexbuf = Lexing.from_string buf;;
+let (sdecl, _) = Parser.program Scanner.token lexbuf;;
+let fullprogram = List.fold_left
+    (fun buildup s ->
+      let chan1 = open_in s in
+      let buf1 = String.create (in_channel_length chan1) in
+      let _ = really_input chan1 buf1 0 (String.length buf1) in
+      buildup ^ buf1)
+    buf
+    sdecl.Ast.incl.Ast.includes;;
+let lexbuf = Lexing.from_string fullprogram;;
+let program = Parser.program Scanner.token lexbuf;;
+let _ =  
+  if print then 
+    let listing = Printer.string_of_program program in
     print_string listing 
-  else *)
-    ignore (Interpret.run program) 
+(*   else *)
+(* ignore (Interpret.run program) *)
       
