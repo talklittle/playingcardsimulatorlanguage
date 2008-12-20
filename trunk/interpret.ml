5140d24f5acb2333d6c8469f429f1f6bb9523657
ehenestroza <ehenestroza@63fd7cd2-9ecc-11dd-af58-c1d6d257e395>
2008-12-20 00:29:02 +0000

    Updated interpreter to fix some bugs, created a simplified poker program.

--- trunk/interpret.ml
+++ trunk/interpret.ml
@@ -70,22 +70,27 @@ let run (program) =
         | CardLiteral(i1),   Neq, CardLiteral(i2)   -> boolean (i1 <> i2)
         | BoolLiteral(i1),   Neq, BoolLiteral(i2)   -> boolean (string_of_bool i1 <> string_of_bool i2)
         | Null,    Equal, Null    -> boolean(true)
+        | Null,    Neq, Null    -> boolean(false)
         | IntLiteral(i1),    Equal, Null    -> boolean(false)
         | StringLiteral(i1), Equal, Null -> boolean(false)
         | CardLiteral(i1),   Equal, Null   -> boolean(false)
+        | Variable(VarExp(id, Entity)),   Equal, Null   -> boolean(false)
         | BoolLiteral(i1),   Equal, Null   -> boolean(false)
-        | IntLiteral(i1),    Neq, Null    -> boolean(false)
-        | StringLiteral(i1), Neq, Null -> boolean(false)
-        | CardLiteral(i1),   Neq, Null   -> boolean(false)
-        | BoolLiteral(i1),   Neq, Null   -> boolean(false)
+        | IntLiteral(i1),    Neq, Null    -> boolean(true)
+        | StringLiteral(i1), Neq, Null -> boolean(true)
+        | CardLiteral(i1),   Neq, Null   -> boolean(true)
+        | Variable(VarExp(id, Entity)),   Neq, Null   -> boolean(true)
+        | BoolLiteral(i1),   Neq, Null   -> boolean(true)
         | Null,    Equal, IntLiteral(i2)    -> boolean(false)
         | Null, Equal, StringLiteral(i2) -> boolean(false)
         | Null,   Equal, CardLiteral(i2)   -> boolean(false)
+        | Null, Equal, Variable(VarExp(id, Entity)) -> boolean(false)
         | Null,   Equal, BoolLiteral(i2)   -> boolean(false)
-        | Null,    Neq, IntLiteral(i2)    -> boolean(false)
-        | Null, Neq, StringLiteral(i2) -> boolean(false)
-        | Null,   Neq, CardLiteral(i2)   -> boolean(false)
-        | Null,   Neq, BoolLiteral(i2)   -> boolean(false)
+        | Null,    Neq, IntLiteral(i2)    -> boolean(true)
+        | Null, Neq, StringLiteral(i2) -> boolean(true)
+        | Null,   Neq, CardLiteral(i2)   -> boolean(true)
+        | Null, Neq, Variable(VarExp(id, Entity)) -> boolean(true)
+        | Null,   Neq, BoolLiteral(i2)   -> boolean(true)
         | IntLiteral(i1),    Less, IntLiteral(i2)    -> boolean (i1 < i2)
         | StringLiteral(i1), Less, StringLiteral(i2) -> boolean (i1 < i2)
         | CardLiteral(i1),   Less, CardLiteral(i2)   -> boolean (i1 < i2) (* cmp cards as string? *)
@@ -338,7 +343,7 @@ let run (program) =
         let actuals, env = List.fold_left
             (fun (actuals, values) actual ->
               let v, env = eval env actual in
-              v :: actuals, values) ([], env) actuals
+              List.append actuals [v], values) ([], env) actuals
         in
         let (locals, globals, entities, cards) = env in
         try
